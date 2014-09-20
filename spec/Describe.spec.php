54f963e523a2f0aaa3f2f77f69721de1c86fec3f
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2014-09-20 23:27:49 +0200

    Some whitespace to make it more readable

--- spec/Describe.spec.php
+++ spec/Describe.spec.php
@@ -3,21 +3,27 @@
 use Webspot\SpotSpec\Describe;
 
 return (new Describe('how Describe describes a spec suite'))
+
     ->beforeEach(function () {
         $this->description = 'a test';
         $this->suite = new Describe($this->description);
     })
+
     ->it('can be created', function () {
         return $this->expect($this->suite instanceof Describe)->toBeTrue();
     })
+
     ->it('can have a property set upon it', function () {
         $value = 'value';
         $this->suite->property = $value;
         return $this->expect($this->suite->property)->toEqual($value);
     })
+
     ->it('has a description', function () {
         return $this->expect($this->suite->getDescription())->toEqual($this->description);
     })
+
     ->it('will fail', function () {
         return $this->expect(true)->toNotBeFalse();
-    });
+    })
+;
